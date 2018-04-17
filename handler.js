67c122dec1e33981a2cba14966876e169e32f04c
Mariana Casella dos Santos <mariana.casella@88solutions.be>
2018-04-17 18:42:39 +0200

    'added final handler"

--- handler.js
+++ handler.js
@@ -1,10 +1,21 @@
 
-let binaryAdditionHandler = function(){
+let convertToBinaryHandler = function(){
     let num = document.getElementById("decimal").value;
     let binary = convertToBinaryStr(num);
     let display = document.getElementById("binary");
-    display.innerHTML = binary
+    display.value = binary
 }
 
 let BTconvertToBinary =document.getElementById('BTconvertToBinary');
-BTconvertToBinary.addEventListener('click', binaryAdditionHandler);
+BTconvertToBinary.addEventListener('click', convertToBinaryHandler);
+
+let binaryAdditionHandler = function(){
+    let num1 = document.getElementById("num1").value;
+    let num2 = document.getElementById("num2").value;
+    let sum = binaryAddition(num1, num2);
+    let display = document.getElementById("sum");
+    display.value = sum;
+}
+
+let BTbinaryAddition = document.getElementById('BTbinaryAddition');
+BTbinaryAddition.addEventListener('click', binaryAdditionHandler);
